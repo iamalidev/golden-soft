8fb8642c2408c93331b1fc0a6b244ddc8c824e26
alisher <alisherotaboyev072@gmail.com>
2023-06-06 23:40:35 +0500

    mobile menu color changed

--- src/components/Header/MobileMenu/style.js
+++ src/components/Header/MobileMenu/style.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 const MobileMenuWrapper = styled.div`
   position: fixed;
-  top: 121px;
+  top: 125px;
   left: 0;
   right: 0;
   bottom: 0;
   width: 100%;
   min-height: 706px;
-  background: white;
+  background: red;
   transition: 0.4s;
   transform: ${({ open }) =>
     open == true ? "translateX(0)" : "translateX(-100%)"};
