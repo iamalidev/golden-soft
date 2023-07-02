f1cd8123f15a85b7756eddaf09780d0e472037fb
alisher <alisherotaboyev072@gmail.com>
2023-07-02 23:03:35 +0500

    cart modal window info styles added

--- src/components/CartModal/index.jsx
+++ src/components/CartModal/index.jsx
@@ -3,8 +3,8 @@ import Backdrop from "@mui/material/Backdrop";
 import Modal from "@mui/material/Modal";
 import * as Style from "./style";
 import CartModalTop from "./CartModalTop";
-import CartModalBox from "./CartModalBox";
-import CartModalBottom from "./CartModalBottom";
+import { ProductRateBox, RateBoxText } from "../ProductCard/style";
+import Rate from "../Rate";
 
 export default function CartModal({ cartModal, handleModal, data }) {
   console.log(data);
@@ -26,10 +26,43 @@ export default function CartModal({ cartModal, handleModal, data }) {
       >
         <Style.CartModalWindow>
           <CartModalTop cartModal={cartModal} handleModal={handleModal} />
-          {data.map((el) => (
-            <CartModalBox key={el.id} product={el} />
-          ))}
-          <CartModalBottom />
+          {data.length ? (
+            <Style.CartModalContent>
+              <Style.CartContentWapper>
+                {data.map((el) => (
+                  <Style.ItemsWrapper key={el.id}>
+                    <Style.ItemsInfo>
+                      <Style.ItemsImgWrapper>
+                        <img src={el.img} alt="" />
+                      </Style.ItemsImgWrapper>
+                      <Style.InfoBox>
+                        <Style.InfoBoxTop>
+                          <Style.InfoTitle>{el.title}</Style.InfoTitle>
+                          <Style.RateBoxWrapper>
+                            <ProductRateBox>
+                              <Rate />
+                              <RateBoxText>(12) отзывов</RateBoxText>
+                            </ProductRateBox>
+                          </Style.RateBoxWrapper>
+                        </Style.InfoBoxTop>
+                      </Style.InfoBox>
+                    </Style.ItemsInfo>
+                  </Style.ItemsWrapper>
+                ))}
+              </Style.CartContentWapper>
+              <Style.CartModalBottomWrapper>
+                <Style.CartModalBottomBtn>
+                  Оформить заказ
+                </Style.CartModalBottomBtn>
+                <Style.TotalWrapper>
+                  <Style.TotalText>Итого:</Style.TotalText>
+                  <Style.TotalPrice>66 000₽</Style.TotalPrice>
+                </Style.TotalWrapper>
+              </Style.CartModalBottomWrapper>
+            </Style.CartModalContent>
+          ) : (
+            <Style.EmptyText>Пока пусто : (</Style.EmptyText>
+          )}
         </Style.CartModalWindow>
       </Modal>
     </div>
--- src/components/CartModal/style.js
+++ src/components/CartModal/style.js
@@ -1,4 +1,5 @@
 import { Box } from "@mui/system";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Colors, Flex, Fonts } from "../../utils";
 import { adaptiveValue } from "../../utils/variable";
@@ -12,9 +13,168 @@ const CartModalWindow = styled(Box)`
   width: 100%;
   height: 80vh;
   background-color: ${Colors.white};
-  ${Flex.flexBetween};
+  ${Flex.justFlex};
   flex-direction: column;
   outline: none;
 `;
 
-export { CartModalWindow };
+const CartContentWapper = styled.div`
+  width: 100%;
+  ${adaptiveValue("padding-top", 24, 13)};
+  ${adaptiveValue("padding-bottom", 130, 125)};
+  ${adaptiveValue("padding-inline", 40, 13)};
+  overflow-y: auto;
+  ${Flex.justFlex}
+  align-items: center;
+  flex-direction: column;
+  gap: 15px;
+  position: relative;
+  top: 0;
+`;
+
+const CartModalContent = styled.div`
+  width: 100%;
+  ${Flex.justFlex}
+  align-items: center;
+  flex-direction: column;
+  overflow-y: auto;
+`;
+
+const ItemsWrapper = styled.div`
+  width: 100%;
+  ${Flex.justFlex};
+`;
+
+const ItemsInfo = styled.div`
+  ${Flex.justFlex}
+  gap: 12px;
+`;
+
+const ItemsImgWrapper = styled.div`
+  ${adaptiveValue("width", 136, 65)};
+  ${adaptiveValue("height", 120, 63)};
+  background-color: #f7f7f7;
+
+  & > img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const InfoBox = styled.div`
+  ${Flex.just};
+  flex-direction: column;
+  gap: 35px;
+`;
+
+const InfoBoxTop = styled.div`
+  ${Flex.just};
+  flex-direction: column;
+  gap: 6px;
+`;
+
+const InfoTitle = styled.p`
+  ${adaptiveValue("width", 303, 140)}
+  font-family: ${Fonts.SFPRegular};
+  ${adaptiveValue("font-size", 16, 11)}
+  ${adaptiveValue("line-height", 26, 18)}
+  color: ${Colors.black};
+`;
+
+const RateBoxWrapper = styled.div`
+  @media (max-width: 576px) {
+    display: none;
+  }
+`;
+
+const CartModalBottomWrapper = styled.div`
+  width: 100%;
+  ${adaptiveValue("padding-block", 26, 13)};
+  ${adaptiveValue("padding-inline", 40, 13)};
+  background-color: ${Colors.white};
+  ${Flex.spaceBetween};
+  border-top: 1px solid #eaeaea;
+  position: fixed;
+  bottom: 0;
+
+  @media (max-width: 700px) {
+    flex-direction: column-reverse;
+    align-items: end;
+    gap: 15px;
+  }
+`;
+
+const CartModalBottomBtn = styled(Link)`
+  width: 249px;
+  padding-block: 10px;
+  background-color: ${Colors.blue};
+  border: 1px solid ${Colors.blue};
+  font-family: ${Fonts.SFPMedium};
+  font-size: 16px;
+  line-height: 26px;
+  color: ${Colors.white};
+  text-align: center;
+  transition: 0.4s;
+
+  &:hover {
+    background-color: transparent;
+    color: ${Colors.blue};
+  }
+
+  @media (max-width: 700px) {
+    width: 100%;
+  }
+`;
+
+const TotalWrapper = styled.div`
+  ${Flex.justFlex};
+  gap: 15px;
+  align-items: end;
+
+  @media (max-width: 700px) {
+    width: 100%;
+    ${Flex.flexBetween}
+  }
+`;
+
+const TotalText = styled.p`
+  font-family: ${Fonts.SFPRegular};
+  ${adaptiveValue("font-size", 16, 12)};
+  ${adaptiveValue("line-height", 26, 20)};
+  color: ${Colors.black};
+`;
+
+const TotalPrice = styled.span`
+  font-family: ${Fonts.SFPSemibold};
+  ${adaptiveValue("font-size", 20, 14)};
+  ${adaptiveValue("line-height", 30, 25)};
+  color: ${Colors.black};
+`;
+
+const EmptyText = styled.p`
+  font-family: ${Fonts.SFPLight};
+  font-size: 16px;
+  line-height: 120%;
+  color: ${Colors.gray};
+  margin: auto;
+`;
+
+export {
+  CartModalWindow,
+  CartContentWapper,
+  CartModalContent,
+  ItemsWrapper,
+  ItemsInfo,
+  ItemsImgWrapper,
+  CartModalBottomWrapper,
+  CartModalBottomBtn,
+  TotalWrapper,
+  TotalText,
+  TotalPrice,
+  EmptyText,
+  InfoBox,
+  InfoBoxTop,
+  InfoTitle,
+  RateBoxWrapper,
+};
