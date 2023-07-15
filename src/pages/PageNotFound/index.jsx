e8a70e711e671c7cc652900db9b2576ea84b532d
alisher <alisherotaboyev072@gmail.com>
2023-07-15 23:14:43 +0500

    added about us and 404 pages

--- src/pages/PageNotFound/index.jsx
+++ src/pages/PageNotFound/index.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "../../components/Container/style";
+import img from "../../assets/img/404.png";
+import * as Style from "./style";
+import { CategoryPageBtn } from "../CategoryPage/style";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <Style.PNF>
+      <Container>
+        <Style.Wrapper>
+          <Style.ImgWrapper>
+            <img src={img} alt="" />
+          </Style.ImgWrapper>
+          <Style.LinkBtn to="/">Перейти на главную</Style.LinkBtn>
+        </Style.Wrapper>
+      </Container>
+    </Style.PNF>
+  );
 };
 
 export default PageNotFound;
