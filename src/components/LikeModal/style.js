09aeaf979f9c564174c5332a6e7339d20616685c
alisher <alisherotaboyev072@gmail.com>
2024-09-23 23:11:26 +0500

    some bugs have been fixed

--- src/components/LikeModal/style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-const ItemsWrapper = styled.div`
-  width: 100%;
-`;
-
-export { ItemsWrapper };
--- src/utils/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Colors } from "./variable";
-export { default as Fonts } from "./fonts";
-export { default as Flex } from "./flex";
